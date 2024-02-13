9a9e85887a55929b408c423ceb21f4a9d2af3e00
Joseangvas <developer.javp@gmail.com>
2024-02-13 19:45:40 -0400

    Crear Componentes CSS

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ const Contenedor = styled.div`
   max-width: 900px;
   margin: 0 auto;
   width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
 `
 
 const Imagen = styled.img`
