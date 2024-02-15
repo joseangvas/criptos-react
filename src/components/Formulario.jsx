1ae5717fda93432ac0772056dd31dd16ad029c43
Joseangvas <developer.javp@gmail.com>
2024-02-15 06:58:12 -0400

    Mejora de CSS Styled

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -31,11 +31,9 @@ const Seleccion = styled.select`
     width: 100%;
     display: block;
     padding: 1rem;
-    -webkit-appearance: none;
     border-radius: 10px;
     border: none;
     font-size: 1.2rem;
-
 `
 
 const Formulario = () => {
