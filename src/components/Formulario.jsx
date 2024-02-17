efcdc75e899f8c74a1d29f7bd67bcc0b88ac15fe
Joseangvas <developer.javp@gmail.com>
2024-02-17 17:33:12 -0400

    Codificar CSS al Hook SelectMonedas

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -11,7 +11,7 @@ const InputSubmit = styled.input`
   color: #FFF;
   font-weight: 700;
   text-transform: uppercase;
-  font-size: 20px;
+  font-size: 18px;
   border-radius: 5px;
   transition: background-color .3s ease;
 
@@ -34,7 +34,7 @@ const Seleccion = styled.select`
     padding: 1rem;
     border-radius: 10px;
     border: none;
-    font-size: 1.2rem;
+    font-size: 1rem;
 `
 
 const Formulario = () => {
