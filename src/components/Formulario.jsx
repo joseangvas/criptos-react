765a7e8dcfab41fdbdac0484a0ed91f48851ebe3
Joseangvas <developer.javp@gmail.com>
2024-02-17 17:44:22 -0400

    Mejoras del Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -14,6 +14,7 @@ const InputSubmit = styled.input`
   font-size: 18px;
   border-radius: 5px;
   transition: background-color .3s ease;
+  margin-top: 20px;
 
   &:hover {
     background-color: #7A7BFE;
