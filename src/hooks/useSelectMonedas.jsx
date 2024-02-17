7e2f009108a595fe4abb72102ba174314ccc5087
Joseangvas <developer.javp@gmail.com>
2024-02-17 17:42:00 -0400

    Codificar CSS del Hook SelectMonedas

--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -5,16 +5,24 @@ const Label = styled.label`
     color: #FFF;
     display: block;
     font-family: 'Lato', sans-serif;
-    font-size: 24px;
+    font-size: 18px;
     font-weight: 700;
     margin: 15px 0;
+`
+
+const Select = styled.select`
+    width: 100%;
+    font-size: 18px;
+    padding: 14px;
+    border-radius: 10px;
+    margin-bottom: 20px;
 `
 
 const useSelectMonedas = (label, opciones) => {
     const SelectMonedas = () => (
         <>
             <Label>{label}</Label>
-            <select>
+            <Select>
                 <option value="">- Seleccione Moneda -</option>
                 {opciones.map(opcion => (
                     <option
@@ -23,7 +31,7 @@ const useSelectMonedas = (label, opciones) => {
                         >{opcion.nombre}
                     </option>
                 ))}
-            </select>
+            </Select>
         </>
     )
 
